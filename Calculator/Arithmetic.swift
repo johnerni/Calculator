fdf197c66cd09630e81aa147267d09d76ca5e132
John Ernest Ramos <john.ramos@erni.ph>
2018-04-11 17:46:55 +0800

    Clean up project - Remove Jenkinsfile - Rename ViewModel to Arithmetic - Clean up storyboard

---
+++ Calculator/Arithmetic.swift
@@ -0,0 +1,28 @@
+//
+//  Arithmetic.swift
+//  Calculator
+//
+//  Created by John Ernest Ramos on 4/3/18.
+//  Copyright © 2018 John Ernest Ramos. All rights reserved.
+//
+
+import Foundation
+
+struct Arithmetic {
+    
+    func add(num1: Int, num2: Int) -> Int {
+        return num1 + num2
+    }
+    
+    func subtract(num1: Int, num2: Int) -> Int {
+        return num1 - num2
+    }
+    
+    func multiply(num1: Int, num2: Int) -> Int {
+        return num1 * num2
+    }
+    
+    func divide(num1: Int, num2: Int) -> Int {
+        return num1 / num2
+    }
+}
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,21 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var viewModel: CalculatorViewModel!
-    @IBOutlet var numberButtons: [UIButton]!
-    @IBOutlet weak var outputLabel: UILabel!
+    var viewModel: Arithmetic!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        viewModel = CalculatorViewModel()
-        //test
-        for button in numberButtons {
-            print("Buttons")
-            button.addTarget(self, action: #selector(typeNumber(sender:)), for: .touchUpInside)
-        }
-        outputLabel.text = "0"
-        // Do any additional setup after loading the view, typically from a nib.
+        viewModel = Arithmetic()
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,39 +23,5 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @objc func typeNumber(sender: UIButton) {
-       
-        if let number = sender.titleLabel?.text {
-            if outputLabel.text == "0" {
-                outputLabel.text = number
-            } else {
-                outputLabel.text?.append(number)
-            }
-        }
-    }
-
-    @IBAction func minus(_ sender: UIButton) {
-        
-        if let number = sender.titleLabel?.text {
-            if outputLabel.text?.last == "+" {
-                outputLabel.text?.removeLast()
-            }
-            outputLabel.text?.append(number)
-            
-        }
-    }
-    @IBAction func add(_ sender: UIButton) {
-        
-        if let number = sender.titleLabel?.text {
-            if outputLabel.text?.last == "-" {
-                outputLabel.text?.removeLast()
-            }
-            outputLabel.text?.append(number)
-            
-        }
-    }
-    @IBAction func compute(_ sender: UIButton) {
-        print("TODO computeasd")
-    }
 }
 
